33dcc6df4f5e866b9b5d00268a1faf48b64a936a
598046453 <598046453@qq.com>
2022-08-12 16:57:20 +0800

    商家端

---
+++ src/main/java/com/cykj/TakeoutShopApplication.java
@@ -0,0 +1,16 @@
+package com.cykj;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.cykj.mapper")
+public class TakeoutShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeoutShopApplication.class, args);
+        System.out.println("http://localhost:9905");
+    }
+
+}
